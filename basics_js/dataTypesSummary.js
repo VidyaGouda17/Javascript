337f583d4d30d734b834909d088f5db1d0c2c75c
VidyaGouda17 <146480594+VidyaGouda17@users.noreply.github.com>
2023-09-29 20:43:20 +0000

    datatypes summary

---
+++ basics_js/dataTypesSummary.js
@@ -0,0 +1,37 @@
+//  Primitive =>  call by value hota hai will not get the original refrence will get the copy of the reference from the memrory
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference types (Non primitive) :- this values are those values will save the refernce in the memory
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
