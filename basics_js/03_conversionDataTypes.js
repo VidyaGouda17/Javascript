98a6877b1e4e51328fbb0dad73fefcc7a7e53aae
VidyaGouda17 <146480594+VidyaGouda17@users.noreply.github.com>
2023-09-29 20:24:35 +0000

    added the conversion data type

--- basics_js/03_conversionDataTypes.js
+++ basics_js/03_conversionDataTypes.js
@@ -1,18 +1,16 @@
-let score = undefined
+let score = "hitesh"
 
 //console.log(typeof score);
 //console.log(typeof(score));
 
 let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
-// null => 0
-// undefined =>  NaN
 
 let isLoggedIn = "hitesh"
 
@@ -22,7 +20,6 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
-//
 
 let someNumber = 33
 
@@ -30,8 +27,46 @@ let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
+// *********************** Operations ***********************
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing. => x++
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing. => ++x
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
